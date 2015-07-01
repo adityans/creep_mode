6ed168a1947d351a066bf9a7a219d7dd393ce950
Rahul Madduluri <rahulmadduluri@gmail.com>
2015-07-01 00:53:49 -0700

    Creep Mode Activated

--- popup.js
+++ popup.js
@@ -1,72 +1,31 @@
-var x = $('a').find('span').filter(':contains("Like")');
-console.log(x);
-var num_buttons = x.length;
-for(var i = 0; i < num_buttons; i++)
+function removeAll(x)
 {
-    x[i].remove();
+	var num_buttons = x.length;
+	for (var i = 0; i < num_buttons; i++)
+	{
+		x[i].remove();
+	}
 }
 
-var y = $('.UFILikeIcon');
-
-num_buttons = y.length;
-
-for(var j = 0; j < num_buttons; j++)
+function doEverything()
 {
-    y[j].remove();
+	var x = $('a').find('span').filter(':contains("Like")');
+	removeAll(x)
+
+	var y = $('.UFILikeIcon');
+	removeAll(y);
+
+	var z = $('.UFILikeLink');
+	removeAll(z);
 }
 
-
-var z = $('.UFILikeLink');
-
-num_buttons = z.length;
-
-for(var k = 0; k < num_buttons; k++)
+function removeNewElements()
 {
-    z[k].remove();
-}
-
-
-
-    
-function removeLikeIcons()
-{
+	doEverything();
     $(document).on('DOMNodeInserted', function(e)
     {
-    	console.log("hi")
-    	console.log(e);
-    	console.log(e.target)
-    	console.log(e.target.className)
-
-    	var element = e.target.find('.UFILikeIcon');
-    	if(element)
-    	{
-    		element.remove()
-    	}
+    	doEverything();
     });
 }
 
-
-function respondToNewElements()
-{
-    removeLikeIcons();
-
-}
-
-respondToNewElements();
-
-
-
-
-function removeLikeButtons()
-{
-    var likeButtons = $('a').find('span').filter(':contains("Like")');
-    var numButtons = likeButtons.length;
-
-    for (var k = 0; k < numButtons; k++)
-    {
-        $(document).on(likeButtons[k].attr('class'), function(e)
-        {
-        	console.log(e)
-        });
-    }
-}
+removeNewElements();
